63f88839bc2c226da96f19dbac834a596d5c6005
DESKTOP-1B2N9JN\caoth <caothibinhminh@gmail.com>
2017-02-22 09:17:12 +0100

    commit person

---
+++ src/personinfo.java
@@ -0,0 +1,28 @@
+/**
+ * Created by caoth on 2/22/2017.
+ */
+public class personinfo {
+
+
+    public personinfo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    String name;
+        int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+}
